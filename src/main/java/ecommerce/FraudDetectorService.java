cc81994407ca982dabad6705c4a75f6414f9e9cc
marlonfurtado <marlonfurtado.rs@gmail.com>
2020-01-05 16:21:28 -0300

    add consumer to get a record

---
+++ src/main/java/ecommerce/FraudDetectorService.java
@@ -0,0 +1,50 @@
+package ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class FraudDetectorService {
+
+    public static void main(String[] args) {
+        var consumer = new KafkaConsumer<String, String>(properties());
+        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
+
+        // to listen
+        while(true) {
+            var records = consumer.poll(Duration.ofMillis(100));
+
+            if (records.isEmpty()) {
+                System.out.println("Records: " + records.count());
+            }
+
+            for(var record : records) {
+                System.out.println("-----------------------------------------");
+                System.out.println("Processing new order, checking for fraud.");
+                System.out.println("key: " + record.key());
+                System.out.println("value: " + record.value());
+                System.out.println("partition: " + record.partition());
+                System.out.println("offset: " + record.offset());
+                System.out.println();
+
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {}
+            }
+        }
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+
+        return properties;
+    }
+}
--- src/main/java/ecommerce/NewOrderMain.java
+++ src/main/java/ecommerce/NewOrderMain.java
@@ -18,7 +18,7 @@ public class NewOrderMain {
         producer.send(record).get();
     }
 
-    public static Properties properties() {
+    private static Properties properties() {
         var properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
